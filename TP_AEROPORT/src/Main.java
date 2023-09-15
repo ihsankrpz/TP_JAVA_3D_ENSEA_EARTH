cfe549b6edb6a941b1192ca0f651dbb11487d694
ihsan <ihsankarpuz61@hotmail.com>
2023-09-15 10:39:46 +0200

    First test

--- TP_AEROPORT/src/Main.java
+++ TP_AEROPORT/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main ( String[] args ) {
 
-        System.out.println ( "Hello world!" );
+        System.out.println ( "This is a first test to try if Git and IntellijIdea can work together" );
     }
 }
